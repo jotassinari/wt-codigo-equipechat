7241783ec3e0712e5e9b7e59ba2a2b09f6976131
Josevã Tassinari <99340042+jotassinari@users.noreply.github.com>
2024-02-10 11:48:50 -0300

    cores netecia

--- frontend/src/layout/index.js
+++ frontend/src/layout/index.js
@@ -53,11 +53,11 @@ const useStyles = makeStyles((theme) => ({
     },
     backgroundColor: theme.palette.fancyBackground,
     '& .MuiButton-outlinedPrimary': {
-      color: theme.mode === 'light' ? '#00BFFF' : '#FFF',
+      color: theme.mode === 'light' ? '#b30e16' : '#FFF',
       border: theme.mode === 'light' ? '1px solid rgba(0 124 102)' : '1px solid rgba(255, 255, 255, 0.5)',
     },
     '& .MuiTab-textColorPrimary.Mui-selected': {
-      color: theme.mode === 'light' ? '#00BFFF' : '#FFF',
+      color: theme.mode === 'light' ? '#b30e16' : '#FFF',
     }
   },
   avatar: {
